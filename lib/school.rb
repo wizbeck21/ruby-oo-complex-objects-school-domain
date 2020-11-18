b0293e48ef2e4929e302d86f3062f8e169dfea83
Wesley <wes.beck21@outlook.com>
2020-11-17 21:49:11 -0800

    commit tree cf9004d23fca42e8ad2e7d8ac2b727baf712e50d

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+    
+    def initialize(school_name, roster = {})
+    @school_name = school_name
+    @roster = roster
+    end
+    def roster
+        @roster
+    end
+# adding a student to the array in the array 'grade' within the hash 'roster'
+    def add_student(student, grade_num)
+        @student = student
+        @grade_num = grade_num
+        @roster[@grade_num] ||= []
+        @roster[@grade_num] << "#{@student}"
+    end
+
+    def grade(grade_num)
+        @grade_num = grade_num
+        @roster[@grade_num]
+    end
+
+    def sort
+        sorted_roster = {}
+        @roster.each do |grade_num, students|
+            sorted_roster[grade_num] =  students.sort
+        end
+        sorted_roster
+    end
+end
